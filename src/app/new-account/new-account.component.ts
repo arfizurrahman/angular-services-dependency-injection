bab6027274fe005ac7bc72b13f053e84c2f3ecae
Arfizur Rahman <arfizrahman0@gmail.com>
2019-03-30 00:48:24 +0600

    Add existing files

---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,23 @@
+import { AccountsService } from './../accounts.service';
+import { LoggingService } from '../logging.service';
+import { Component, } from '@angular/core';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+  // providers: [LoggingService]
+})
+export class NewAccountComponent {
+  constructor(private loggingService: LoggingService,
+    private accountsService: AccountsService){
+      accountsService.statusUpdated.subscribe(
+        (status: string) => alert('This alert was thrown' + status)
+      );
+    }
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountsService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
+  }
+}
